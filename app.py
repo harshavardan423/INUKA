b79f71cd06e2fe950b97a216f13b14cead1dc905
harshavardan423 <harshavardan423@gmail.com>
2024-01-16 21:03:49 +0530

    sqllite_version

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from app import app
 import os
-from whitenoise import WhiteNoise
 
 port = int(os.environ.get("PORT", 5000))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port,debug=False)
-    app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
     

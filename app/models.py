83bb0e7e061bfdfb6f0e75e75814d700c3832663
harshavardan423 <harshavardan423@gmail.com>
2024-01-16 13:18:26 +0530

    update_createengine

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ from sqlalchemy import text
 from sqlalchemy import create_engine
 connection_string = "mysql+mysqlconnector://s93lvdgilc09ysjxvpfu:pscale_pw_rwDc6jbpEATVbFKd8KjSh3ilEckfPAMNEbwy3FVAt9h@aws.connect.psdb.cloud:3306/inuka"
 # "mysql+mysqlconnector://ca0e8ywnnxof110pu46x:pscale_pw_TalLclSTAsu0ikmws676YNXISJMO3BF2uj4XFsFXXoI@aws.connect.psdb.cloud:3306/sqlalchemy"
-engine = create_engine(connection_string, echo=True)
+engine = create_engine()
 
 db = SQLAlchemy()
 

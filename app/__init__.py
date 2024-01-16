4e1dad0ff0de18aefc2e057c323d418fe76fe157
harshavardan423 <harshavardan423@gmail.com>
2024-01-16 20:59:52 +0530

    sqllite_version

--- app/__init__.py
+++ app/__init__.py
@@ -12,10 +12,15 @@ password = os.environ.get("DB_PASSWORD")
 secret_key = secrets.token_hex(24)
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inuka_db.sqlite3'
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{username}:{password}@aws.connect.psdb.cloud:3306/inuka"
-# 'mysql+pymysql://your_planetscale_username:your_planetscale_password@your_planetscale_host:3306/inuka'
-# 'sqlite:///inuka_db.sqlite3'
+
+# Configure Flask app based on DEBUG flag
+if os.environ.get("DEBUG") == "TRUE":
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inuka_db.sqlite3'
+    app.config['DEBUG'] = True
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{username}:{password}@aws.connect.psdb.cloud:3306/inuka"
+    app.config['DEBUG'] = False
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = secret_key  # Set a strong and secure secret key
 db.init_app(app)
@@ -23,13 +28,9 @@ db.init_app(app)
 # Import your routes after initializing the app
 from . import routes  # Use a relative import
 
-
-
 # Create tables when the application starts
 with app.app_context():
     db.create_all()
 
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=os.environ.get("DEBUG") == "TRUE")

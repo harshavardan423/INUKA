81fc215b1c6995898be179e1234ad28369645226
harshavardan423 <harshavardan423@gmail.com>
2024-01-16 16:43:48 +0530

    updated_pwd

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ password = os.environ.get("DB_PASSWORD")
 secret_key = secrets.token_hex(24)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://nxoptfmnxcjb1z4v7m86:pscale_pw_GjeaCTfDMsU1gS8D1YHsBtjxOOW6rWPcmC7IAh80bxK@aws.connect.psdb.cloud:3306/inuka"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://tk77gwrcldsuov2jt0sq:pscale_pw_mEEFEHCcYSwvJZRhITrIqm9IOSSlryD0moVocktZt8t@aws.connect.psdb.cloud:3306/inuka"
 # 'mysql+pymysql://your_planetscale_username:your_planetscale_password@your_planetscale_host:3306/inuka'
 # 'sqlite:///inuka_db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
